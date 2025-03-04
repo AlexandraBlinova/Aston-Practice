aad9a55328ea34ac9b5c56a07448cefa05783674
AlexandraBlinova <testieduab@gmail.com>
2025-03-04 19:46:19 +0100

    Delete Lesson_5 directory

--- Lesson_5/src/main/java/org/example/Main.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Product product = new Product("Car", "28.02.2025", "Audi", "Germany", 2800, true);
-    }
-}
