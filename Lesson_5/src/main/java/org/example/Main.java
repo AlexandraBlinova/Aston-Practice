b87dd347bedbe8209313e556998edc5d527d550b
AlexandraBlinova <testieduab@gmail.com>
2025-03-04 19:47:02 +0100

    Add files via upload

---
+++ Lesson_5/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Product[] productsArr = new Product[5];
+        productsArr[0] = new Product("Car", "13.04.2019", "Toyota", "Japan", 230, true);
+        productsArr[1] = new Product("Car", "01.11.1995", "Hyundai", "South Korea", 245, true);
+        productsArr[2] = new Product("Car", "22.03.2023", "Volkswagen", "Germany", 130, false);
+        productsArr[3] = new Product("Car", "08.04.2002", "Audi", "Germany", 210, true);
+        productsArr[4] = new Product("Car", "18.02.2003", "Nissan", "Japan", 500, true);
+
+    }
+}
